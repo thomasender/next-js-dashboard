04f5654ca0e42a84f332508a4af0cc2ce55df1cc
Thomas Ender <freisingen@yahoo.com>
2024-06-09 20:27:42 +0200

    create, update, delete with server Actions, awesome

--- app/lib/definitions.ts
+++ app/lib/definitions.ts
@@ -16,14 +16,16 @@ export type Customer = {
   image_url: string;
 };
 
+// In TypeScript, this is called a string union type.
+// It means that the "status" property can only be one of the two strings: 'pending' or 'paid'.
+export type InvoiceStatus = 'pending' | 'paid';
+
 export type Invoice = {
   id: string;
   customer_id: string;
   amount: number;
-  date: string;
-  // In TypeScript, this is called a string union type.
-  // It means that the "status" property can only be one of the two strings: 'pending' or 'paid'.
-  status: 'pending' | 'paid';
+  date: string
+  status: InvoiceStatus
 };
 
 export type Revenue = {
